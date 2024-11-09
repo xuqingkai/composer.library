9dff79f0777b2109741f3d9060caad0d81108039
徐清凯 <xuqingkai@hotmail.com>
2024-11-09 10:36:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 include_once('vendor/autoload.php');
-use xuqingkai\Index;
+use xuqingkai\Demo;
 
-$result = Index::index();
+$result = Demo::index();
 if(gettype($result)=='array' || gettype($result)=='object'){
     exit(json_encode($result, JSON_UNESCAPED_UNICODE));
 }else{
