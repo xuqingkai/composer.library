31687151dfb0bc07bc04fd63d1f543d7f34d76a2
徐清凯 <xuqingkai@hotmail.com>
2024-11-09 17:26:18 +0800

    Update Alipay.php

--- src/pay/Alipay.php
+++ src/pay/Alipay.php
@@ -3,7 +3,21 @@ namespace xuqingkai\pay
 
 class Alipay{
   
-  protected $alipay=[];
+  protected $alipay=[
+    'version'=>'2.0',
+    'url'=>'https://openapi.alipay.com/gateway.do',
+    'app_id'=>'',
+    'app_key'=>'',
+    'app_secret_key'=>'',
+    'rsa_private_key'=>'',
+    'rsa_public_key'=>'',
+    'notify_url'=>'http://www.xuqingkai.com/pay/notify',
+    'return_url'=>'http://www.xuqingkai.com/pay/return',
+    'charset'=>'utf-8',
+    'sign_type'=>'RSA2',
+    'timeout'=>'30',
+    'debug'=>false,
+  ];
   
   public function __construct($config=[]){
     $this->config($config);
