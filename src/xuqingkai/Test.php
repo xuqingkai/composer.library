5191c2082c56da52e854a14304db95c1e684f9f1
徐清凯 <xuqingkai@hotmail.com>
2024-11-09 10:58:24 +0800

    Update and rename src/xuqingkai/Test.php to src/xuqingkai/test/Test.php

--- src/xuqingkai/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace xuqingkai;
-
-class Test
-{
-    public static function show(){
-        return "去Demo中调用Test的show方法";
-    }
-}
---
+++ src/xuqingkai/test/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace xuqingkai\test;
+
+class Test
+{
+    public static function show(){
+        return "去Demo中调用test\Test的show方法";
+    }
+}
