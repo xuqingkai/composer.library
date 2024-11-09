58b5edfc38ce9c620f7386543fe89b68d926f3af
徐清凯 <xuqingkai@hotmail.com>
2024-11-09 11:35:19 +0800

    Update Alipay.php

--- src/xuqingkai/pay/Alipay.php
+++ src/xuqingkai/pay/Alipay.php
@@ -12,18 +12,18 @@ class Alipay{
     $this->alipay=array_merge($this->alipay, $value);
   }
   public function pc(){
-    
+    return 'http://www.xuqingkai.com/pay/pc';
   }
   public function h5(){
-    
+    return 'http://www.xuqingkai.com/pay/h5';
   }
   public function qr(){
-    
+    return 'http://www.xuqingkai.com/pay/qr';
   }
   public function scan(){
-    
+    return 'http://www.xuqingkai.com/pay/scan';
   }
   public function app(){
-    
+    return 'http://www.xuqingkai.com/pay/app';
   }
 }
